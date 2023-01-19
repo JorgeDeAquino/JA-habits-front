598e544617737d9e56110c46c7173157027ceba5
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-19 15:59:40 -0300

    first commit

---
+++ JA-habits-front/src/App.tsx
@@ -0,0 +1,16 @@
+import { Habit } from './components/Habit';
+
+function App() {
+  return (
+    <div>
+      <Habit />
+      <Habit />
+      <Habit />
+      <Habit />
+      <Habit />
+      <Habit />
+    </div>
+  )
+}
+
+export default App
